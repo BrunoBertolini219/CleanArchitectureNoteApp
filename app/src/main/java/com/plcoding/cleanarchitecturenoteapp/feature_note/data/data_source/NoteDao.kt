ed3d0e9187d6e92e695f4f5202cb0e922ae8f3a4
Bruno Bertolini <bruno.bertolini@hotmail.com>
2021-11-30 20:32:30 -0300

    chore: modules structure added

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDao.kt
@@ -0,0 +1,23 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source
+
+import androidx.room.*
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    fun insetNotes(note: Note)
+
+    @Query("SELECT * FROM note")
+    fun getNotes(): Flow<List<Note>>
+
+    @Query("SELECT * FROM note WHERE id = :id")
+    suspend fun getNoteById(id: Int): Note?
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+
+}
